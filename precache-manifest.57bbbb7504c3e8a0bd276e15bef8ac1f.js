72d240e68a1c9738442a84941608388ac9560356
AndreyKryzhniov <andreykryzhniov@gmail.com>
2020-02-17 01:14:08 +0300

    Updates

---
+++ precache-manifest.57bbbb7504c3e8a0bd276e15bef8ac1f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "919764687791cf7def73a5adac827860",
+    "url": "/shop_mini/index.html"
+  },
+  {
+    "revision": "a3640e1bd68b3435522a",
+    "url": "/shop_mini/static/css/main.9351153c.chunk.css"
+  },
+  {
+    "revision": "cf589b28dd4cd99ee24e",
+    "url": "/shop_mini/static/js/2.ce71db1e.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/shop_mini/static/js/2.ce71db1e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a3640e1bd68b3435522a",
+    "url": "/shop_mini/static/js/main.2f19cff6.chunk.js"
+  },
+  {
+    "revision": "8fc65a5a31c8d6927e29",
+    "url": "/shop_mini/static/js/runtime-main.4d818050.js"
+  },
+  {
+    "revision": "00dacddca2191c236e87a9f148949db8",
+    "url": "/shop_mini/static/media/comandir.00dacddc.mp3"
+  },
+  {
+    "revision": "07a3367d3286fea814f04418267e3486",
+    "url": "/shop_mini/static/media/loader.07a3367d.svg"
+  }
+]);
